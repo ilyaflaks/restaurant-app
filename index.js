e6ea42edbf324ee30a6d30f602636db7d1c7ec94
Ilya Flaks <105448137+ilyaflaks@users.noreply.github.com>
2022-10-06 13:50:40 -0700

    banana

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 
 //app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.static(path.join(__dirname, "./app")));
+app.use(express.static(path.join(__dirname, "./banana")));
 
 const typeDefs = gql`
   type Book {
