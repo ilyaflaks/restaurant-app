b59ac212e92e14ece3f809046e028c60a6a98afb
Ilya Flaks <105448137+ilyaflaks@users.noreply.github.com>
2022-10-06 14:07:55 -0700

    no bananass

--- index.js
+++ index.js
@@ -115,9 +115,7 @@ app.post("/payment", cors(), async (req, res) => {
 
 app.get("*", (req, res) => {
   console.log("--------------------THIS IS NEW-");
-  console.log(
-    path.join(__dirname, "app", ".next", "server", "pages", "index.html")
-  );
+  console.log(path.join(__dirname, ".next", "server", "pages", "index.html"));
   // res.send("My app");
   //  res.sendFile(path.join(__dirname, ".next", "server", "pages", "index.html"));
 });
