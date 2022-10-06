e9526bbac833c9996f811be3663928c2b825593d
Ilya Flaks <105448137+ilyaflaks@users.noreply.github.com>
2022-10-06 13:16:45 -0700

    better paths

--- index.js
+++ index.js
@@ -115,8 +115,9 @@ app.post("/payment", cors(), async (req, res) => {
 });
 
 app.get("*", (req, res) => {
+  console.log("---------------------");
   // res.send("My app");
-  res.sendFile(__dirname, "./client/server/pages/index.html");
+  res.sendFile(__dirname, "./client/.next/server/pages/index.html");
 });
 
 app.listen(PORT, function () {
